adb7b9329161bd1e04ac44fd352b6cf5de0b28d8
Fajar Arief <fajararahman11@gmail.com>
2023-11-14 02:29:51 +0000

    final dbt

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -1,11 +1,18 @@
 {{ config(materialized='view') }}
- 
-select 
- -- identifiers
+
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by cast(vendorid as integer), cast(tpep_pickup_datetime as timestamp) order by tpep_pickup_datetime) as rn
+  from {{ source('staging','yellow_taxi_trips') }}
+  where vendorid is not null 
+)
+select
+   -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
-    cast(pulocationid as integer) as  pickup_locationid,
+    cast(pulocationid as integer) as pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
     
     -- timestamps
@@ -31,9 +38,8 @@ select
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
- 
- from {{ source('staging','yellow_taxi_trips') }}
-where vendorid is not null
+from tripdata
+where rn = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
